377af0ad03822fdc1257461378dcce99b95c3f35
David Briscoe <idbrii@gmail.com>
2025-12-10 13:38:00 -0800

    Add a player

--- game/autoloads/c.gd
+++ game/autoloads/c.gd
@@ -2,11 +2,14 @@
 extends "res://addons/popochiu/engine/interfaces/i_character.gd"
 
 # classes ----
+const PCBasil := preload("res://game/characters/basil/character_basil.gd")
 # ---- classes
 
 # nodes ----
+var Basil: PCBasil : get = get_Basil
 # ---- nodes
 
 # functions ----
+func get_Basil() -> PCBasil: return get_runtime_character("Basil")
 # ---- functions
 
