a88c7e555b8202b2fc7bf9331891ba353379147a
David Briscoe <idbrii@gmail.com>
2025-12-10 14:29:21 -0800

    Add a room and setup walkable area
    I can hit play and see the player walk around!

--- game/autoloads/r.gd
+++ game/autoloads/r.gd
@@ -2,11 +2,14 @@
 extends "res://addons/popochiu/engine/interfaces/i_room.gd"
 
 # classes ----
+const PRBedroom01 := preload("res://game/rooms/bedroom_01/room_bedroom_01.gd")
 # ---- classes
 
 # nodes ----
+var Bedroom01: PRBedroom01 : get = get_Bedroom01
 # ---- nodes
 
 # functions ----
+func get_Bedroom01() -> PRBedroom01: return get_runtime_room("Bedroom01")
 # ---- functions
 
---
+++ game/rooms/bedroom_01/room_bedroom_01.gd
@@ -0,0 +1,30 @@
+@tool
+extends PopochiuRoom
+
+const Data := preload('room_bedroom_01_state.gd')
+
+var state: Data = load("res://game/rooms/bedroom_01/room_bedroom_01.tres")
+
+
+#region Virtual ####################################################################################
+# What happens when Popochiu loads the room. At this point the room is in the
+# tree but it is not visible
+func _on_room_entered() -> void:
+	pass
+
+
+# What happens when the room changing transition finishes. At this point the room
+# is visible.
+func _on_room_transition_finished() -> void:
+	# You can use await E.queue([]) to execute a sequence of instructions
+	pass
+
+
+# What happens before Popochiu unloads the room.
+# At this point, the screen is black, processing is disabled and all characters
+# have been removed from the $Characters node.
+func _on_room_exited() -> void:
+	pass
+
+
+#endregion
