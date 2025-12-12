0135adcb12eaf3121dcf287138428d95f1207a80
David Briscoe <idbrii@gmail.com>
2025-12-12 12:45:08 -0800

    Popochiu: Layout dialogue above heads and wrapped
    This wasn't constraining the width of the text, so set x as well. Use
    the target to figure out where to position, but we need to adjust for
    the size of the text so it still lines up with the "Dialog Position".
    There's some default height and using 150 makes it line up okay.

--- game/gui/components/dialog_text/dialog_caption/dialog_caption.gd
+++ game/gui/components/dialog_text/dialog_caption/dialog_caption.gd
@@ -2,16 +2,18 @@ extends PopochiuDialogText
 
 
 #region Private ####################################################################################
-func _modify_size(msg: String, _target_position: Vector2) -> void:
-	var _size := await _calculate_size(msg)
-	
+func _modify_size(msg: String, target_position: Vector2) -> void:
+	var computed_size := await _calculate_size(msg)
+
 	# Define size and position (before calculating overflow)
-	rich_text_label.size.y = _size.y
-	rich_text_label.position.y = get_meta(DFLT_POSITION).y - (_size.y - get_meta(DFLT_SIZE).y)
+
+	# HACK: This wasn't constraining the width of the text, so set x as well.
+	# Use the target to figure out where to position, but we need to adjust for
+	# the size of the text so it still lines up with the "Dialog Position".
+	# There's some default height and using 150 makes it line up okay.
+	rich_text_label.size = computed_size
+	rich_text_label.position = target_position + Vector2(-computed_size.x/2, 150.0 - computed_size.y/2)
 
 
 func _append_text(msg: String, props: Dictionary) -> void:
 	rich_text_label.text = "[center][color=%s]%s[/color][/center]" % [props.color.to_html(), msg]
-
-
-
