122f33b929d9c220381752f0ff76138a4a451e89
David Briscoe <idbrii@gmail.com>
2025-12-12 13:35:24 -0800

    Allow entering pirate room

--- game/rooms/hall_01/hotspots/pirate_door/hotspot_pirate_door.gd
+++ game/rooms/hall_01/hotspots/pirate_door/hotspot_pirate_door.gd
@@ -9,18 +9,18 @@ extends PopochiuHotspot
 
 # Interact: When the node is left clicked.
 func _on_click() -> void:
-    await RoomUtil.look_door("It's locked.")
-    #~ await RoomUtil.use_door(R.NauticalRoom)
+    #~ await RoomUtil.look_door("It's locked.")
+    await RoomUtil.use_door(R.NauticalRoom)
 
 
 func _on_double_click() -> void:
-    await _on_click()
-    #~ await RoomUtil.teleport_door(R.NauticalRoom)
+    #~ await _on_click()
+    await RoomUtil.teleport_door(R.NauticalRoom)
 
 
 # Look: When the node is right clicked.
 func _on_right_click() -> void:
-    await RoomUtil.look_door("The {0} is under repairs\nafter the last guest brought a [shake]cannon[/shake].".format([InteractUtil.get_room_pretty_name(R.NauticalRoom)]))
+    await RoomUtil.look_door("The {0} isn't quite the same since the last guest brought a [shake]cannon[/shake].".format([InteractUtil.get_room_pretty_name(R.NauticalRoom)]))
 
 
 # When the node is middle clicked
