bc160e3248e983dea947af60bda13b1a14ca9a03
David Briscoe <idbrii@gmail.com>
2025-12-12 14:53:47 -0800

    Setup second joke

--- game/autoloads/d.gd
+++ game/autoloads/d.gd
@@ -6,6 +6,7 @@ const PDPhonesHallChat := preload("res://game/dialogs/phones_hall_chat/dialog_ph
 const PDEvanHomeNoiseComplaint := preload("res://game/dialogs/evan_home_noise_complaint/dialog_evan_home_noise_complaint.gd")
 const PDDapperHomeJoke := preload("res://game/dialogs/dapper_home_joke/dialog_dapper_home_joke.gd")
 const PDDapperHomeIntro := preload("res://game/dialogs/dapper_home_intro/dialog_dapper_home_intro.gd")
+const PDDapperHomeJoke2 := preload("res://game/dialogs/dapper_home_joke_2/dialog_dapper_home_joke_2.gd")
 # ---- classes
 
 # nodes ----
@@ -13,6 +14,7 @@ var PhonesHallChat: PDPhonesHallChat : get = get_PhonesHallChat
 var EvanHomeNoiseComplaint: PDEvanHomeNoiseComplaint : get = get_EvanHomeNoiseComplaint
 var DapperHomeJoke: PDDapperHomeJoke : get = get_DapperHomeJoke
 var DapperHomeIntro: PDDapperHomeIntro : get = get_DapperHomeIntro
+var DapperHomeJoke2: PDDapperHomeJoke2 : get = get_DapperHomeJoke2
 # ---- nodes
 
 # functions ----
@@ -20,4 +22,5 @@ func get_PhonesHallChat() -> PDPhonesHallChat: return get_instance("PhonesHallCh
 func get_EvanHomeNoiseComplaint() -> PDEvanHomeNoiseComplaint: return get_instance("EvanHomeNoiseComplaint")
 func get_DapperHomeJoke() -> PDDapperHomeJoke: return get_instance("DapperHomeJoke")
 func get_DapperHomeIntro() -> PDDapperHomeIntro: return get_instance("DapperHomeIntro")
+func get_DapperHomeJoke2() -> PDDapperHomeJoke2: return get_instance("DapperHomeJoke2")
 # ---- functions
