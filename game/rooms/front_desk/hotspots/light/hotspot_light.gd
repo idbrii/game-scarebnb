fc46fff8ef5e022dfb00526daf721aa71235ddda
David Briscoe <idbrii@gmail.com>
2025-12-12 00:00:45 -0800

    Add front desk room
    I setup the light and calendar, but their clickable areas don't seem to
    work.

---
+++ game/rooms/front_desk/hotspots/light/hotspot_light.gd
@@ -0,0 +1,54 @@
+@tool
+extends PopochiuHotspot
+# You can use E.queue([]) to trigger a sequence of events.
+# Use await E.queue([]) if you want to pause the execution of
+# the function until the sequence of events finishes.
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Virtual ░░░░ {{{1
+
+# Interact: When the node is left clicked.
+func _on_click() -> void:
+    await InteractUtil.approach_and_say("I guess I'm done for the day.")
+
+
+# Teleport: Node is double left clicked.
+func _on_double_click() -> void:
+    # Replace the call to E.command_fallback() with your code.
+    PopochiuUtils.e.command_fallback()
+    # For example, you could make the player instantly goto another room on an exit hotspot instead
+    # of waiting for the player to walk there.
+#    await RoomUtil.teleport_door(R.NewRoom)
+
+
+# Look: When the node is right clicked.
+func _on_right_click() -> void:
+    await InteractUtil.face_and_say("I can turn out the light when I'm ready to retire for the night.")
+
+
+# Unused.
+func _on_middle_click() -> void:
+    # Replace the call to E.command_fallback() to implement your code.
+    PopochiuUtils.e.command_fallback()
+
+
+# When the node is clicked and there is an inventory item selected
+func _on_item_used(_item: PopochiuInventoryItem) -> void:
+    # Replace the call to E.command_fallback() to implement your code.
+    PopochiuUtils.e.command_fallback()
+    # For example, you can make the player character say something when the Key item is used in this
+    # hotspot. Note that you have to change the name of the `_item` parameter to `item`.
+#    if item == I.Key:
+#        await C.player.say("No can do")
+
+
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Public ░░░░ {{{1
+# You can add here functions triggered by the GUI commands. For example, if your GUI has a command
+# for look_at, you could have the function:
+#func on_look_at() -> void:
+    #pass
+
+
+
