23f7832b69f282108b310c53c003f4ad4dfad8bf
David Briscoe <idbrii@gmail.com>
2025-12-12 14:13:03 -0800

    Setup dapper's intro and first joke

--- game/autoloads/d.gd
+++ game/autoloads/d.gd
@@ -4,14 +4,20 @@ extends "res://addons/popochiu/engine/interfaces/i_dialog.gd"
 # classes ----
 const PDPhonesHallChat := preload("res://game/dialogs/phones_hall_chat/dialog_phones_hall_chat.gd")
 const PDEvanHomeNoiseComplaint := preload("res://game/dialogs/evan_home_noise_complaint/dialog_evan_home_noise_complaint.gd")
+const PDDapperHomeJoke := preload("res://game/dialogs/dapper_home_joke/dialog_dapper_home_joke.gd")
+const PDDapperHomeIntro := preload("res://game/dialogs/dapper_home_intro/dialog_dapper_home_intro.gd")
 # ---- classes
 
 # nodes ----
 var PhonesHallChat: PDPhonesHallChat : get = get_PhonesHallChat
 var EvanHomeNoiseComplaint: PDEvanHomeNoiseComplaint : get = get_EvanHomeNoiseComplaint
+var DapperHomeJoke: PDDapperHomeJoke : get = get_DapperHomeJoke
+var DapperHomeIntro: PDDapperHomeIntro : get = get_DapperHomeIntro
 # ---- nodes
 
 # functions ----
 func get_PhonesHallChat() -> PDPhonesHallChat: return get_instance("PhonesHallChat")
 func get_EvanHomeNoiseComplaint() -> PDEvanHomeNoiseComplaint: return get_instance("EvanHomeNoiseComplaint")
+func get_DapperHomeJoke() -> PDDapperHomeJoke: return get_instance("DapperHomeJoke")
+func get_DapperHomeIntro() -> PDDapperHomeIntro: return get_instance("DapperHomeIntro")
 # ---- functions
---
+++ game/dialogs/dapper_home_intro/dialog_dapper_home_intro.gd
@@ -0,0 +1,74 @@
+@tool
+extends PopochiuDialog
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Virtual ░░░░ {{{1
+
+func _on_start() -> void:
+    # something to execute before showing the dialog options.
+    # (!) It MUST always use an await
+    await E.queue([
+        "DapperGhost: ‘Ey there kid, where’s a recently dead vaudeville comedian to get a drink around here?",
+        ])
+    await PopochiuUtils.e.get_tree().process_frame
+
+
+func _option_selected(opt: PopochiuDialogOption) -> void:
+    # You can make the player character say the selected option with:
+    #~ await D.say_selected()
+
+    var followup = [
+        "DapperGhost:(whispered) [i](C’mon kid… I’m settin’ you up for GOLD here.)[/i]",
+        "Player: Gold? Like a tip?",
+        "DapperGhost: [shake]What? No![/shake] I mean COMEDY gold! You’re supposed to say somethin’ like “I know we’ve got a lotta thirsty ghosts but we don’t serve BOOS here!”",
+        "Player: …",
+        "DapperGhost: And here I thought [i]I[/i] was lifeless. Y’know, ever since my comedy partner went solo into the afterlife, I can’t help but feel… outta step. If I could do a zippy routine again, with a partner who had [b]PUNCHLINES[/b]–maybe [i]then[/i] I could find some peace…",
+        "Player: Oh! BOOS as in BOOZE! Yeah we don’t have that here.",
+        "DapperGhost: … I’m gonna spend eternity here, aren’t I?",
+    ]
+
+    # Use match to check which option was selected and execute something for
+    # each one
+    match opt.id:
+        "DRINK":
+            await E.queue([
+                "Player: I don’t know.",
+                "DapperGhost: Err, well, uh, does this room have a mini bar, perchance?",
+                "Player: … no.",
+            ])
+            await E.queue(followup)
+            C.DapperGhost.has_seen_intro = true
+            stop()
+
+        "BAR":
+            await D.say_selected()
+            await E.queue([
+                "DapperGhost: So this room has a mini bar, perchance?",
+                "Player: … no.",
+            ])
+            await E.queue(followup)
+            C.DapperGhost.has_seen_intro = true
+            stop()
+
+        _:
+            await C.player.say("Have a wonderful time!")
+            # By default close the dialog. Options won't show after calling
+            # stop()
+            stop()
+
+    _show_options()
+
+
+# Use this to save custom data for this PopochiuDialog when saving the game.
+# The Dictionary must contain only JSON supported types: bool, int, float, String.
+func _on_save() -> Dictionary:
+    return {}
+
+
+# Called when the game is loaded.
+# This Dictionary should has the same structure you defined for the returned one in _on_save().
+func _on_load(data: Dictionary) -> void:
+    prints(data)
+
+
+
